5ee724576fe0a4bf9679d5aac2cffefbf81086cb
Denis <denkolmykov@gmail.com>
2024-12-19 19:17:35 +0100

    Changed ft_atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dkolmyko <dkolmyko@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 15:05:44 by dkolmyko          #+#    #+#             */
-/*   Updated: 2024/12/19 17:11:44 by dkolmyko         ###   ########.fr       */
+/*   Updated: 2024/12/19 18:07:41 by dkolmyko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,37 +37,39 @@ int	ft_atoi(const char *str)
 	return (result * sign);
 }
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <limits.h>
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-// // Replace this with your implementation
-// int	ft_atoi(const char *str);
+// Replace this with your implementation
+int	ft_atoi(const char *str);
 
-// int main(void)
-// {
-// 	printf("Test 1: '42' -> %d (Expected: 42)\n", \
-// 	       ft_atoi("42"));
-// 	printf("Test 2: '   -42' -> %d (Expected: -42)\n", \
-// 	       ft_atoi("   -42"));
-// 	printf("Test 3: '4193 with words' -> %d (Expected: 4193)\n", \
-// 	       ft_atoi("4193 with words"));
-// 	printf("Test 4: 'words and 987' -> %d (Expected: 0)\n", \
-// 	       ft_atoi("words and 987"));
-// 	printf("Test 5: '-91283472332' -> %d (Expected: %d)\n", \
-// 	       ft_atoi("-91283472332"), INT_MIN);
-// 	printf("Test 6: '91283472332' -> %d (Expected: %d)\n", \
-// 	       ft_atoi("91283472332"), INT_MAX);
-// 	printf("Test 7: '2147483647' -> %d (Expected: 2147483647)\n", \
-// 	       ft_atoi("2147483647"));
-// 	printf("Test 8: '-2147483648' -> %d (Expected: -2147483648)\n", \
-// 	       ft_atoi("-2147483648"));
-// 	printf("Test 9: '--42' -> %d (Expected: 0)\n", \
-// 	       ft_atoi("--42"));
-// 	printf("Test 10: '+-42' -> %d (Expected: 0)\n", \
-// 	       ft_atoi("+-42"));
-// 	printf("Test 11: '0' -> %d (Expected: 0)\n", \
-// 	       ft_atoi("0"));
+int main(void)
+{
+	printf("Test 1: '42' -> %d (Expected: 42)\n", \
+	       ft_atoi("42"));
+	printf("Test 2: '   -42' -> %d (Expected: -42)\n", \
+	       ft_atoi("   -42"));
+	printf("Test 3: '4193 with words' -> %d (Expected: 4193)\n", \
+	       ft_atoi("4193 with words"));
+	printf("Test 4: 'words and 987' -> %d (Expected: 0)\n", \
+	       ft_atoi("words and 987"));
+	printf("Test 5: '-91283472332' -> %d (Expected: %d)\n", \
+	       ft_atoi("-91283472332"), INT_MIN);
+	printf("Test 6: '91283472332' -> %d (Expected: %d)\n", \
+	       ft_atoi("91283472332"), INT_MAX);
+	printf("Test 7: '2147483647' -> %d (Expected: 2147483647)\n", \
+	       ft_atoi("2147483647"));
+	printf("Test 8: '-2147483648' -> %d (Expected: -2147483648)\n", \
+	       ft_atoi("-2147483648"));
+	printf("Test 9: '--42' -> %d (Expected: 0)\n", \
+	       ft_atoi("--42"));
+	printf("Test 10: '+-42' -> %d (Expected: 0)\n", \
+	       ft_atoi("+-42"));
+	printf("Test 11: '0' -> %d (Expected: 0)\n", \
+	       ft_atoi("0"));
 
-// 	return (0);
-// }
+	return (0);
+}
+*/
