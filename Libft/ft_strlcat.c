5fdcfe4e4616a5622860df13125b55b3eef6c2a0
Denis <denkolmykov@gmail.com>
2024-12-19 17:12:42 +0100

    Edited ft_atoi & ft_strlcat

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,41 +6,57 @@
 /*   By: dkolmyko <dkolmyko@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 12:07:30 by dkolmyko          #+#    #+#             */
-/*   Updated: 2024/12/17 16:05:51 by dkolmyko         ###   ########.fr       */
+/*   Updated: 2024/12/19 16:14:35 by dkolmyko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *restrict src, size_t dsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dsize)
 {
 	size_t	i;
-	size_t	len;
+	size_t	dst_len;
+	size_t	src_len;
 
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dsize <= dst_len)
+		return (dsize + src_len);
 	i = 0;
-	len = ft_strlen(dst);
-	if (dsize == 0)
-		return (ft_strlen(src));
-	while (src[i] != '\0' && (len + i + 1) < dsize)
+	while (src[i] != '\0' && (dst_len + i + 1) < dsize)
 	{
-		dst[len + i] = src[i];
+		dst[dst_len + i] = src[i];
 		i++;
 	}
-	if (len + i < dsize)
-	{
-		dst[len + i] = '\0';
-	}
-	return (ft_strlen(src) + len);
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
 
-// int main()
-// {
-// 	char dst[20] = "Hello, ";
-// 	const char *src = "World!";
-// 	// size_t result = strlcat(dst, src, 0);
-// 	size_t result = ft_strlcat(dst, src, 0);
+// #include <stdio.h>
+// #include <string.h>
 
-// 	printf("Buffer: %s\n", dst);		   // Outputs "Hello, World!"
-// 	printf("Total length: %zu\n", result);
+// // Replace this with your implementation
+// size_t	ft_strlcat(char *dst, const char *src, size_t dsize);
+
+// int main(void)
+// {
+// 	char buffer[20];
+// 	memset(buffer, 0, sizeof(buffer));
+// 	strcpy(buffer, "Hello");
+
+// 	// Test Case 1: Appending to `dst`
+// 	printf("Return Value: %zu\n", ft_strlcat(buffer, " World", sizeof(buffer)));
+//Exp:11
+// 	printf("Resulting String: %s\n", buffer); // Expected: "Hello World"
+
+// 	// Test Case 2: `dsize` smaller than `dst`
+// 	strcpy(buffer, "Hello");
+// 	printf("Return Value: %zu\n", ft_strlcat(buffer, " World", 5)); // Exp: 11
+// 	printf("Resulting String: %s\n", buffer); // Expected: "Hello" (unchanged)
+
+// 	// Test Case 3: `dsize` is 0
+// 	printf("Return Value: %zu\n", ft_strlcat(buffer, " World", 0)); // Exp: 6
+// 	printf("Resulting String: %s\n", buffer); // Expected: "Hello" (unchanged)
+
 // 	return 0;
 // }
