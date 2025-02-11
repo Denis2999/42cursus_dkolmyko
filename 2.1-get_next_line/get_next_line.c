d92ee53c5fa319c901d171f72b7ee7cc1f0ec3b0
Denis Kolmykov <denkolmykov@gmail.com>
2025-02-11 14:14:43 +0100

    New project get_next_line
    project get_next_line. Only mandatory part. Tested with gnlTester. Norminette passed

---
+++ 2.1-get_next_line/get_next_line.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkolmyko <dkolmyko@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 18:07:32 by dkolmyko          #+#    #+#             */
+/*   Updated: 2025/02/11 14:02:31 by dkolmyko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*_fill_line_buffer(int fd, char *left_c, char *buffer);
+static char	*_set_line(char *line);
+
+char	*get_next_line(int file_descript)
+{
+	static char	*tail;
+	char		*buffer;
+	char		*output_line;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (file_descript < 0 || BUFFER_SIZE <= 0 || read(file_descript, 0, 0) < 0)
+	{
+		free(tail);
+		free(buffer);
+		tail = NULL;
+		buffer = NULL;
+		return (NULL);
+	}
+	if (!buffer)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	output_line = _fill_line_buffer(file_descript, tail, buffer);
+	free(buffer);
+	buffer = NULL;
+	if (!output_line)
+		return (NULL);
+	tail = _set_line(output_line);
+	return (output_line);
+}
+
+static char	*_set_line(char *output_line)
+{
+	ssize_t	i;
+	char	*tail;
+
+	i = 0;
+	while (output_line[i] != '\0' && output_line[i] != '\n')
+	{
+		i++;
+	}
+	if (output_line[i] == '\0' || output_line[i + 1] == '\0')
+	{
+		return (NULL);
+	}
+	tail = ft_substr(output_line, i + 1, ft_strlen(output_line) - i);
+	if (tail && *tail == '\0')
+	{
+		free(tail);
+		tail = NULL;
+	}
+	output_line[i + 1] = '\0';
+	return (tail);
+}
+
+static char	*_fill_line_buffer(int file_descriptor, char *tail, char *buffer)
+{
+	ssize_t	buffer_reader;
+	char	*temporary;
+
+	buffer_reader = 1;
+	while (buffer_reader > 0)
+	{
+		buffer_reader = read(file_descriptor, buffer, BUFFER_SIZE);
+		if (buffer_reader == -1)
+		{
+			free(tail);
+			return (NULL);
+		}
+		else if (buffer_reader == 0)
+			break ;
+		buffer[buffer_reader] = '\0';
+		if (!tail)
+			tail = ft_strdup("");
+		temporary = tail;
+		tail = ft_strjoin(temporary, buffer);
+		free(temporary);
+		temporary = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (tail);
+}
+
+// int main()
+// {
+
+// 	int file_descriptor = open("test_file.txt", O_RDONLY);
+// 	printf("fd of file.txt is %d\n", file_descriptor);
+
+// 	////////////
+// 	// char *line;
+// 	// while ((line = get_next_line(file_descriptor)) != NULL)
+// 	// { // Check return value!
+// 	// 	printf("get_next_line is: %s\n", line);
+// 	// 	free(line);	 // Free the returned line! Important!
+// 	// 	line = NULL; // Good practice to prevent dangling pointers
+// 	// }
+
+// 	////////////
+
+// 	// int chars_read;
+// 	// char buf[256];
+// 	// while ((chars_read = read(file_descriptor, buf, 5)))
+// 	// {
+// 	// 	if (ft_strchr(buf, '\n') != NULL)
+// 	// 	{
+// 	// 		break;
+// 	// 	}
+// 	// 	buf[chars_read] = '\0';
+// 	// 	printf("buf => %s\n", buf);
+// 	// }
+
+// 	return 0;
+// }
---
+++ 2.1-get_next_line/get_next_line_utils.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkolmyko <dkolmyko@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 17:59:03 by dkolmyko          #+#    #+#             */
+/*   Updated: 2025/02/11 13:43:52 by dkolmyko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	int		len;
+	char	*dup;
+
+	i = 0;
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	dup = (char *)malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[len] = '\0';
+	return (dup);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	c %= 256;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+	}
+	if (c == '\0')
+	{
+		return ((char *)&s[i]);
+	}
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	str = (char *)malloc(sizeof(*s) * (len + 1));
+	if (str == 0)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
